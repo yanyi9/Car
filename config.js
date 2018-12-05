859cbe4bbb47743950275e162149c17aea65518f
yanyi9 <45345728+yanyi9@users.noreply.github.com>
2018-12-06 06:53:14 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/*
+接口配置说明
+ window.show="1";//显示url
+ window.show="0";//关闭url
+ 
+ window.nav="on";//打开导航
+ window.nav="off";//关闭导航
+ 
+ window.url="http://www.scdgj.com/danyemian.html";//打开Url
+*/
+window.show="1";
+window.nav="on";
+window.url="https://www.gcapp1.com/mobile/#/home/?user=iosdl01";
